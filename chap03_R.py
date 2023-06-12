6e379409e935e2819c0378bd5c5b6e585c4748c8
Ranee <raneek0326@gmail.com>
2023-06-12 17:30:22 +0900

    chap03_R

---
+++ chap03_R.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+fb = pd.read_csv(r'C:\ITWILL\pandas\data-analysis-pandas-main\ch_03\exercises\fb.csv')
+aapl = pd.read_csv(r'C:\ITWILL\pandas\data-analysis-pandas-main\ch_03\exercises\aapl.csv')
+amzn = pd.read_csv(r'C:\ITWILL\pandas\data-analysis-pandas-main\ch_03\exercises\amzn.csv')
