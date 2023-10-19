47ac75d02d0569344b1e3b3dc63a67c7ff4dd9f2
Ranee <raneek0326@gmail.com>
2023-10-19 21:12:58 +0900

    chap04완

--- chap04.py
+++ chap04.py
@@ -93,3 +93,64 @@ standard_deviation = (sum_of_differences / (len(values) - 1)) ** 0.5
 # ⅲ) event : ['Disappointing user growth announced after colse.', 'Cambridge','Analytica story', 'FTC invsetigation']
 # b) 인덱스를 ['date','ticker']로 설정한다.
 # c) 이 데이터를 FAANG 데이터와 외부 결합으로 병합한다.
+
+events = pd.DataFrame({
+    'ticker': 'FB',
+    'date': pd.to_datetime(
+         ['2018-07-25', '2018-03-19', '2018-03-20']
+    ), 
+    'event': [
+         'Disappointing user growth announced after close.',
+         'Cambridge Analytica story',
+         'FTC investigation'
+    ]
+}).set_index(['date', 'ticker'])
+
+faang.reset_index().set_index(['date', 'ticker']).join(
+    events, how='outer'
+).sample(10, random_state=0)
+
+# 9
+faang = faang.reset_index().set_index(['ticker', 'date'])
+faang_index = (faang / faang.groupby(level='ticker').transform('first'))
+
+# view 3 rows of the result per ticker
+faang_index.groupby(level='ticker').agg('head', 3)
+
+# 10-1
+covid = pd.read_csv('../../ch_04/exercises/covid19_cases.csv')\
+    .assign(date=lambda x: pd.to_datetime(x.dateRep, format='%d/%m/%Y'))\
+    .set_index('date')\
+    .replace('United_States_of_America', 'USA')\
+    .replace('United_Kingdom', 'UK')\
+    .sort_index()
+
+# 10-2
+top_five_countries = covid\
+    .groupby('countriesAndTerritories').cases.sum()\
+    .nlargest(5).index
+
+covid[covid.countriesAndTerritories.isin(top_five_countries)]\
+    .groupby('countriesAndTerritories').cases.idxmax()
+
+# 10-3
+covid\
+    .groupby(['countriesAndTerritories', pd.Grouper(freq='1D')]).cases.sum()\
+    .unstack(0).diff().rolling(7).mean().last('1W')[top_five_countries]
+
+# 10-4
+covid\
+    .pivot(columns='countriesAndTerritories', values='cases')\
+    .drop(columns='China')\
+    .apply(lambda x: x[x > 0].index.min())\
+    .sort_values()\
+    .rename(lambda x: x.replace('_', ' '))
+
+
+# 10-5
+covid\
+    .pivot_table(columns='countriesAndTerritories', values='cases', aggfunc='sum')\
+    .T\
+    .transform('rank', method='max', pct=True)\
+    .sort_values('cases', ascending=False)\
+    .rename(lambda x: x.replace('_', ' '))
